7dc479736204dfa712354dd694009810fa0c0b29
Britta Kotov <brittakotov@gmail.com>
2021-05-09 11:24:50 -0700

    reverse linked list

---
+++ ReverseLinkedList.js
@@ -0,0 +1,12 @@
+function reverse(head) {
+  let prev = null;
+  let next = null;
+
+  while (head !== null) {
+    next = head.next;
+    head.next = prev;
+    prev = head;
+    head = next;
+  }
+  return prev;
+}
